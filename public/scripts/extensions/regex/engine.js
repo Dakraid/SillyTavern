9aed0e52b6baa2e1d0295e8c69cebc3644f7f0cb
Kristan Schlikow <kristian@schlikow.de>
2024-03-09 19:06:26 +0100

    Add workflow to automatically sync fork
    Remove unused workflows for this repo, update sync
    Ignore release branch
    
    Update sync-fork.yml
    Extend range of temp slider for Cohere
    
    Remove old workflows
    
    Add workflow to automatically update the instance on remote server
    
    Update deployment.yml
    Add option to only run Regex on first greeting message
    
    Disable old placement update for 4
    
    Reorder placement for Greetings in Affects in Regex Editor
    
    Fix fav scrolling
    
    Add optional userIdentifier config field for OpenRouter
    
    Update deployment.yml
    
    Experiment to see if adding Python allows ST MCP to use it
    
    Fix potential pipeline killer
    
    fix: Readability of FAL.AI model names
    
    fix: Regression from merging the second Fal model list fix in
    
    Clean-up Release Branch and Actions
    Updates branch merge to use local origin branches instead of upstream

--- public/scripts/extensions/regex/engine.js
+++ public/scripts/extensions/regex/engine.js
@@ -286,6 +286,7 @@ export const regex_placement = {
     USER_INPUT: 1,
     AI_OUTPUT: 2,
     SLASH_COMMAND: 3,
+    GREETING: 4,
     // 4 - sendAs (legacy)
     WORLD_INFO: 5,
     REASONING: 6,
